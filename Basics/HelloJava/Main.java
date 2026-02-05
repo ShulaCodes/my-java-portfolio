e4633672fefd7944ee5ffafe7996404533701887
ShulaCodes <shulaeast@gmail.com>
2026-02-05 12:27:40 -0800

    Enhanced HelloJava with motivational messages

---
+++ Basics/HelloJava/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, everyone!");
+        System.out.println("I am learning Java programming from scratch.");
+        System.out.println("I know this journey would be fun and exciting!");
+        System.out.println("I am ready to start my adventure with Java.");
+        System.out.println("Let's explore the world of Java together!");
+        System.out.println("Come along and join me as I build my skills step by step.");
+        System.out.println("I wish my self happy coding and continuous growth!");
+
+    }
+}
