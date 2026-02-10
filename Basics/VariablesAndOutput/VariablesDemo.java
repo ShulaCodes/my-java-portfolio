81b628a8d584c8abbbdf9504dc795e997816595c
ShulaCodes <shulaeast@gmail.com>
2026-02-10 12:58:12 -0800

    Add variables and output basics in Java

---
+++ Basics/VariablesAndOutput/VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo{
+    public static void main(String[] args) {
+    
+        int age = 19;
+        double gpa = 3.75;
+        String name = "Shulammite";
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Is Student: " + isStudent);
+
+
+    }
+}
