92cdbc537e53469c9601e5eb72e399afd8f183cf
ShulaCodes <shulaeast@gmail.com>
2026-02-10 12:42:07 -0800

    Rename Main.java to HelloJava.java

--- Basics/HelloJava/HelloJava.java
+++ Basics/HelloJava/HelloJava.java
@@ -1,4 +1,4 @@
-public class Main {
+public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hi, everyone!");
         System.out.println("I am learning Java programming from scratch.");
