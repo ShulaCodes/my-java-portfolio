9b8845915c7669eff1c8ccd6398b0e7604014d00
ShulaCodes <shulaeast@gmail.com>
2026-02-26 04:18:47 -0800

    Added gitignore rules

---
+++ Basics/VariablesAndOutput/MyInfo.java
@@ -0,0 +1,14 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "Shulammite";
+        int level = 200;
+        String favLang = "Python and yet to find out more, maybe including Java, laughs";
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Level: " + level);
+        System.out.println("Favorite programming language: " + favLang);
+
+
+    }
+}
